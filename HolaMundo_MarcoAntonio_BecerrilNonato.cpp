5714a1d6b6ac20d55b54d3ac83b0bad32a454e0c
MarcoBN10 <80008776+MarcoBN10@users.noreply.github.com>
2021-03-03 16:45:48 -0600

    Agregando programa

---
+++ HolaMundo_MarcoAntonio_BecerrilNonato.cpp
@@ -0,0 +1,11 @@
+//La biblioteca principal
+#include<stdio.h>
+
+//La función principal
+int main(){
+	
+	printf("\n Hola Marco Antonio Becerril Nonato");
+	
+	//La salida del main 
+    return 0;
+}
